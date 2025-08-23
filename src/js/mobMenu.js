db7f50692611e1037f802b4c7eed399d1519c9be
Bohdan Kret <kretbohdan2002@gmail.com>
2025-08-24 00:11:55 +0300

    create header and mob menu

--- src/js/mobMenu.js
+++ src/js/mobMenu.js
@@ -1,11 +1,43 @@
-const openBtnEl = document.querySelector('[data-action="open"]');
-const closeBtnEl = document.querySelector('[data-action="close"]');
-const burgerMenuEl = document.querySelector('[data-visible]');
+const mobMenu = document.getElementById('mobMenu');
+const backdrop = document.getElementById('backdrop');
+const openBtn = document.getElementById('openMenuBtn');
+const closeBtn = document.getElementById('closeMenuBtn');
+const links = mobMenu.querySelectorAll('a');
 
-openBtnEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'open';
+openBtn.addEventListener('click', onOpenMenuClick);
+
+closeBtn.addEventListener('click', onCloseMenuClick);
+
+backdrop.addEventListener('click', onCloseMenuClick);
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    const targetId = link.getAttribute('href');
+    if (targetId.startsWith('#')) {
+      e.preventDefault();
+      const section = document.querySelector(targetId);
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+    onCloseMenuClick();
+  });
 });
 
-closeBtnEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'close';
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape') {
+    onCloseMenuClick();
+  }
 });
+
+function onOpenMenuClick() {
+  backdrop.classList.add('is-open');
+  mobMenu.classList.add('is-open');
+  openBtn.classList.add('visually-hidden');
+  closeBtn.classList.remove('visually-hidden');
+}
+
+function onCloseMenuClick() {
+  backdrop.classList.remove('is-open');
+  mobMenu.classList.remove('is-open');
+  closeBtn.classList.add('visually-hidden');
+  openBtn.classList.remove('visually-hidden');
+}
